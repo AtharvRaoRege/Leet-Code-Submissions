edba70ac13b1222e668482b3293ff46231177a75
AtharvRaoRege <153847963+AtharvRaoRege@users.noreply.github.com>
2024-06-24 08:26:17 +0530

    Time: 1 ms (99.34%), Space: 42 MB (52.32%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.java
@@ -0,0 +1,32 @@
+class Solution {
+    public static boolean isVowel(char a){
+        a = Character.toLowerCase(a);
+        if(!Character.isLetter(a)) return false;
+        if(a == 'a' || a == 'e' || a == 'i'|| a == 'o'|| a == 'u') return true;
+        return false;
+    }
+    public static boolean isDigit(char ch){
+        if(ch>='0' && ch<='9') return true;
+        return false;
+    }
+    public static boolean isConsonent(char ch){
+        ch = Character.toLowerCase(ch);
+        if(!Character.isLetter(ch)) return false;
+        String vowels = "aeiou";
+        if(vowels.indexOf(ch) != -1) return false;// vowel check
+        return true;
+    }
+    public boolean isValid(String word) {
+        if(word.length() < 3) return false;
+        boolean digits = false,vowels = false, consonent = false;
+        for(int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if(isDigit(ch)) digits = true;
+            else if(isVowel(ch)) vowels = true;
+            else if(isConsonent(ch)) consonent = true;
+            else return false;
+        }
+        if(vowels && consonent) return true;
+        return false;
+    }
+}
