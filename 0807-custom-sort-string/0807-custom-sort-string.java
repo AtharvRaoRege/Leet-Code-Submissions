a30245f44d70858811bfeb3f67968d2279d988f1
AtharvRaoRege <153847963+AtharvRaoRege@users.noreply.github.com>
2024-06-26 10:54:13 +0530

    Time: 2 ms (70.74%), Space: 41 MB (97.47%) - LeetHub

---
+++ 0807-custom-sort-string/0807-custom-sort-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String customSortString(String order, String s) {
+          Map<Character,Integer> hm = new HashMap<>();
+        for(char i: s.toCharArray()){
+            hm.put(i ,hm.getOrDefault(i ,0) + 1);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < order.length(); i++){
+            if(hm.containsKey(order.charAt(i))){
+                int cnt = hm.get(order.charAt(i));
+                while(cnt -- > 0) {
+                    sb.append(order.charAt(i));
+                }
+            }
+        }
+        for(int i = 0; i < s.length(); i++){
+            char x = s.charAt(i);
+            if(order.indexOf(x) == -1) {
+                sb.append(x);
+            }
+        }
+        return sb.toString();  
+    }
+}
